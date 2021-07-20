d3c2835fe6c59363c37c33ec2fb0e8e85da81b1e
xrisaD <xrdikonimaki@hotmail.com>
2021-07-20 14:19:36 +0300

    subword2word

--- gr_nlp_toolkit/processors/tokenizer.py
+++ gr_nlp_toolkit/processors/tokenizer.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from torch.utils.data import DataLoader, Dataset
 
 from gr_nlp_toolkit.document.dataset import DatasetImpl
@@ -40,12 +40,16 @@ def remove_special_tokens(input_ids: List[int]) -> List[int]:
     return input_ids_without_special_tokens
 
 
-def create_mask_and_tokens(input_tokens: List[str], input_ids: List[int]) -> Tuple[List[str], List[Token]]:
+def create_mask_and_tokens(input_tokens: List[str], input_ids: List[int]) -> Tuple[List[str], List[Token], Dict]:
     mask = []
     tokens = []
+    subword2word = {}
 
+    word = 0
     # for each token
-    for t, i in zip(input_tokens, input_ids):
+    for j, input in enumerate(zip(input_tokens, input_ids), 1):
+        t = input[0]
+        i = input[1]
         # it isn't a sub-word
         if not t.startswith("##"):
             #  create a token object
@@ -53,17 +57,19 @@ def create_mask_and_tokens(input_tokens: List[str], input_ids: List[int]) -> Tup
             tokenObj._ids.append(i)
             tokens.append(tokenObj)
             mask.append('0')
+            word = word + 1
         else:
             # add sub-words to token
             tokenObj.subwords.append(t)
             tokenObj._ids.append(i)
             mask.append('1')
+        subword2word[j] = word
 
     # create text
     for token in tokens:
         token.text = create_text(token._ids)
 
-    return mask, tokens
+    return mask, tokens, subword2word
 
 
 def create_dataset_and_dataloader(input_ids) -> Tuple[Dataset, DataLoader]:
@@ -84,7 +90,8 @@ class Tokenizer(AbstractProcessor):
         # create ids
         doc.input_ids = create_ids(doc.text)
         # create mask and tokens
-        doc.mask, doc.tokens = create_mask_and_tokens(convert_to_tokens(doc.input_ids), remove_special_tokens(doc.input_ids))
+        doc.mask, doc.tokens, doc.subword2word = create_mask_and_tokens(convert_to_tokens(doc.input_ids),
+                                                                        remove_special_tokens(doc.input_ids))
 
         # create dataloader
         doc.dataset, doc.dataloader = create_dataset_and_dataloader(doc.input_ids)
