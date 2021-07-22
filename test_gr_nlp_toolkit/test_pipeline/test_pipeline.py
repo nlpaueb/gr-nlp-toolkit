f1eb389a16eb672cf75bc1a6bb96845f88ea5357
Nick Smyr <smyrnioudis.nikolaos@outlook.com>
2021-07-22 11:30:01 +0300

    New test case

--- test_gr_nlp_toolkit/test_pipeline/test_pipeline.py
+++ test_gr_nlp_toolkit/test_pipeline/test_pipeline.py
@@ -9,26 +9,68 @@ from gr_nlp_toolkit.pipeline.pipeline import Pipeline
 class TestPipeline(unittest.TestCase):
     def test_using_all_processors(self):
         nlp = Pipeline('dp,pos,ner')
+
+        sentences = ["Η Ιταλία κέρδισε την Αγγλία στον τελικό του Euro το 2021",
+                     "Το ποιηματάκι το έγραψε ο διάσημος ποιητής, Νίκος Νικολαϊδης"]
+        for sent in sentences:
+            doc = nlp(sent)
+
+            for token in doc.tokens:
+                print(token.text, token.ner, token.upos, token.feats, token.head, token.deprel)
+                self.assertIsNotNone(token.ner)
+                self.assertTrue(token.ner in I2L_IOBES_18)
+                self.assertIsNotNone(token.head)
+                self.assertIsNotNone(token.deprel)
+                # We have to add plus one, because the cls token is removed
+                self.assertTrue(token.head in range(0, len(doc.tokens) + 1))
+                self.assertTrue(token.deprel in I2L_deprels)
+                self.assertIsNotNone(token.upos)
+                self.assertTrue(token.upos in I2L_POS['upos'])
+
+                self.assertIsNotNone(token.feats)
+                self.assertEqual(len(list(token.feats.keys())), len(properties_POS[token.upos]))
+
+                for feat, value in token.feats.items():
+                    self.assertTrue(feat in properties_POS[token.upos])
+                    self.assertTrue(value in I2L_POS[feat])
+
+    def test_annotations_are_same_with_multiple_configurations(self):
+        nlp = Pipeline('dp,pos,ner')
         doc = nlp("Η Ιταλία κέρδισε την Αγγλία στον τελικό του Euro το 2021")
 
+        deprels_preds = []
+        upos_preds = []
+        ner_preds = []
         for token in doc.tokens:
-            print(token.text, token.ner, token.upos, token.feats, token.head, token.deprel)
-            self.assertIsNotNone(token.ner)
-            self.assertTrue(token.ner in I2L_IOBES_18)
-            self.assertIsNotNone(token.head)
-            self.assertIsNotNone(token.deprel)
-            # We have to add plus one, because the cls token is removed
-            self.assertTrue(token.head in range(0, len(doc.tokens) + 1))
-            self.assertTrue(token.deprel in I2L_deprels)
-            self.assertIsNotNone(token.upos)
-            self.assertTrue(token.upos in I2L_POS['upos'])
+            deprels_preds.append(token.deprel)
+            upos_preds.append(token.upos)
+            ner_preds.append(token.ner)
 
-            self.assertIsNotNone(token.feats)
-            self.assertEqual(len(list(token.feats.keys())), len(properties_POS[token.upos]))
+        nlp = Pipeline('dp')
+        doc = nlp("Η Ιταλία κέρδισε την Αγγλία στον τελικό του Euro το 2021")
+        new_deprels_preds = []
 
-            for feat, value in token.feats.items():
-                self.assertTrue(feat in properties_POS[token.upos])
-                self.assertTrue(value in I2L_POS[feat])
+        for token in doc.tokens:
+            new_deprels_preds.append(token.deprel)
+
+        nlp = Pipeline('pos')
+        doc = nlp("Η Ιταλία κέρδισε την Αγγλία στον τελικό του Euro το 2021")
+        new_upos_preds =[]
+
+        for token in doc.tokens:
+            new_upos_preds.append(token.upos)
+
+        nlp = Pipeline('ner')
+        doc = nlp("Η Ιταλία κέρδισε την Αγγλία στον τελικό του Euro το 2021")
+        new_ner_preds =[]
+        for token in doc.tokens:
+            new_ner_preds.append(token.ner)
+
+        self.assertEqual(new_deprels_preds, deprels_preds)
+        self.assertEqual(new_upos_preds, upos_preds)
+        self.assertEqual(new_ner_preds, ner_preds)
+
+
 
     def test_using_only_one_processor(self):
         nlp = Pipeline('ner')
