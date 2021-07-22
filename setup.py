ca105a8fb7bc2e836d981e8d9be163d8ff2256e2
Nick Smyr <smyrnioudis.nikolaos@outlook.com>
2021-07-22 19:10:37 +0300

    Non pinned dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gr-nlp-toolkit",
-    version="0.0.2",
+    version="0.0.3",
     author="nlpaueb",
     author_email="p3170148@aueb.gr, p3170039@aueb.gr",
     description="A Transformer-based Natural Language Processing Pipeline for Greek",
@@ -25,10 +25,10 @@ setuptools.setup(
     packages=setuptools.find_packages(where=".", exclude="./test_gr_nlp_toolkit"),
     python_requires=">=3.6",
     install_requires=[
-         'torch>=1.9.0',
-         'numpy>=1.21.0',
-         'pytorch_wrapper>=1.1.0',
-         'transformers>=4.8.2',
-         'gdown>=3.13.0',
+         'torch',
+         'numpy',
+         'pytorch_wrapper',
+         'transformers',
+         'gdown',
     ]
 )
