a886aff9c835c43f039a3078be11f21c534a1d42
eloukas <129952098+lefterisloukasHelvia@users.noreply.github.com>
2025-01-08 22:17:41 +0200

    Bump version to 0.2.0 and update description for improved clarity

--- setup.py
+++ setup.py
@@ -5,28 +5,28 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gr-nlp-toolkit",
-    version="0.1.0",
+    version="0.2.0",
     author="nlpaueb",
     author_email="p3170148@aueb.gr, p3170039@aueb.gr, spirosbarbakos@gmail.com,  eleftheriosloukas@aueb.gr, ipavlopoulos@aueb.gr",
-    description="A Transformer-based Natural Language Processing Pipeline for Greek",
+    description="The state-of-the-art NLP toolkit for (modern) Greek",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nlpaueb/gr-nlp-toolkit",
     project_urls={
-        #"Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        # "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Text Processing :: Linguistic",
-        "Natural Language :: Greek"
+        "Natural Language :: Greek",
     ],
-    packages=setuptools.find_packages(where=".", exclude="./test_gr_nlp_toolkit"),
+    packages=setuptools.find_packages(where=".", exclude="./tests"),
     python_requires=">=3.9",
     install_requires=[
-         'torch>=2.1.2',
-         'transformers>=4.11.1',
-         'huggingface_hub'
-    ]
+        "torch>=2.1.2",
+        "transformers>=4.11.1",
+        "huggingface_hub>=0.23.5",
+    ],
 )
